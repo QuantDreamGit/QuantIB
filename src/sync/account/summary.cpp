ea470f3ff96e62f8ed50f1863de7c14f2647295c
QuantDreamGit <priolo.giuseppe01@proton.me>
2026-06-09 15:47:01 +0200

    refactor account summary subscription handling and improve logging

--- src/sync/account/summary.cpp
+++ src/sync/account/summary.cpp
@@ -4,7 +4,7 @@ void IB::accountSummarySub(
 	const std::string &tags,
 	const std::string &groups) {
 
-	hub_->subscribe<AccountSummaryTag, AccountSummary>([&]() {
+	hub_->subscribe<AccountSummary>([&]() {
 		client_->reqAccountSummary(nextId_++, groups, tags);
 	});
 
@@ -12,25 +12,24 @@ void IB::accountSummarySub(
 }
 
 void IB::accountSummaryCancel() const {
-	const int reqId = obj_->get<AccountSummaryTag, AccountSummary>().reqId;
+	const int reqId = obj_->get<AccountSummary>().reqId;
 	client_->cancelAccountSummary(reqId);
 	LOG_DEBUG_TAG(IB_STR, "Account Summary subscription cancelled.");
 }
 
-void ResponseWrapper::accountSummary(int reqId, const std::string &account,
+void ResponseWrapper::accountSummary(const int reqId, const std::string &account,
                                      const std::string &tag, const std::string &value,
                                      const std::string &currency) {
 	// Update object
-	auto new_obj = obj_->get<AccountSummaryTag, AccountSummary>();
+	// Since it's a subscription we know that obj is already created
+	auto new_obj = obj_->get<AccountSummary>();
 	new_obj.reqId = reqId;
-	new_obj.account = account;
-	new_obj.currency = currency;
-	new_obj.attributes[tag] = value;
-	LOG_TRACE_TAG(IB_STR, "Account Summary update. Tag: {} Value: {}.", tag, value);
+	new_obj.attributes[tag] = SummaryAttributes(account, value, currency);
+	LOG_TRACE_TAG(IB_STR, "Account Summary update. [Tag: {}] [Value: {}].", tag, value);
 	// Send new version
-	hub_->send<AccountSummaryTag>(new_obj);
+	obj_->update(new_obj);
 }
 
 void ResponseWrapper::accountSummaryEnd(int reqId) {
-	LOG_TRACE_TAG(IB_STR, "Account Summary update complete.");
+	LOG_TRACE_TAG(IB_STR, "Account Summary subscription ended. [ReqId: {}].", reqId);
 }
