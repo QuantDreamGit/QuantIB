474147f1d70992ff534e25aa4820d47e58bab632
QuantDreamGit <priolo.giuseppe01@proton.me>
2026-06-19 14:33:57 +0200

    configurable riskmanager

--- include/config.h
+++ include/config.h
@@ -1,5 +1,11 @@
 #pragma once
+#include "quantib/risk_manager/risk_manager.h"
 
-// I can enable tick logging to see all the ticks received from IB.
-// This is useful for debugging and testing
-// #define ENABLE_TICK_LOGGING
+// In logger.hpp you can choose if tick data should be displayed with ENABLE_TICK_LOGGING
+
+using MyRiskManager = RiskManager<
+		ContractReady<>,					// Check if contract is ready
+		OnlyTradingHours<>,					// Check if order is within trading hours
+		MaxOrderNotional<1000, Side::Buy>,	// Max Notional for long side
+		MaxOrderNotional<1000, Side::Sell> 	// Max Notional for short side
+	>;
