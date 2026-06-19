134f7d0816c3ea8915dcd5877413c592a0e56949
QuantDreamGit <priolo.giuseppe01@proton.me>
2026-06-19 14:33:15 +0200

    added strategy manager

---
+++ include/quantib/core/strategy_manager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+#include "Contract.h"
+#include "EClientSocket.h"
+#include "object_hub.hpp"
+#include "quantib/network/connection.hpp"
+#include "quantib/utils/logger.hpp"
+
+#include "Order.h"
+#include "OrderState.h"
+#include "Execution.h"
+
+class StrategyManager {
+public:
+	StrategyManager(EClientSocket &client, BlockingHub &hub, ObjectHub &obj, Logger &logger,
+	                MyRiskManager &risk) : logger_(logger), client_(client), obj_(obj), hub_(hub), risk_(risk) {
+	}
+
+private:
+	Logger &logger_;
+	EClientSocket &client_;
+	ObjectHub &obj_;
+	BlockingHub &hub_;
+	MyRiskManager &risk_;
+};
