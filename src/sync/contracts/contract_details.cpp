38a1e63f5c12ceb0accc57689bb45d9e873d7447
QuantDreamGit <priolo.giuseppe01@proton.me>
2026-06-19 14:30:41 +0200

    added contract manager

--- src/sync/contracts/contract_details.cpp
+++ src/sync/contracts/contract_details.cpp
@@ -1,7 +1,7 @@
 #include "quantib/sync/ib.hpp"
 
 std::optional<std::vector<ContractDetails> >
-IB::getContractDetails(int reqId, const Contract &contract) const {
+IB::getContractDetailsSync(int reqId, const Contract &contract) const {
 	auto result = hub_->wait_for<ContractDetailsTag, std::vector<ContractDetails> >([&]() {
 		client_->reqContractDetails(reqId, contract);
 	});
@@ -12,25 +12,34 @@ IB::getContractDetails(int reqId, const Contract &contract) const {
 	return result;
 }
 
+std::optional<ContractDetails> IB::getContractDetails(const Contract &contract) const {
+	if (contract.conId != 0) return contract_->getContractDetails(contract.conId);
+	if (contract.symbol != "") return contract_->getContractDetails(contract.symbol);
+	return std::nullopt;
+}
+
+std::optional<ContractDetails> IB::getContractDetails(const std::string &symbol) const {
+	return contract_->getContractDetails(symbol);
+}
+
 void ResponseWrapper::contractDetails(int reqId, const ContractDetails &contractDetails) {
-	obj_.try_append<ContractDetailsTag>(contractDetails);
+	if (obj_.contains<ContractDetailsTag>()) obj_.try_append<ContractDetailsTag>(contractDetails);
+	// Assign contract to contract manager
+	// I'd like to register contracts also coming from sync request
+	obj_.insert_or_assign<ContractStoreTag, int, ContractDetails>(contractDetails.contract.conId, contractDetails);
+	obj_.insert_or_assign<ContractSymbolStoreTag, std::string, ContractInfo>(
+		contractDetails.contract.symbol,
+		ContractInfo(contractDetails.contract.conId, false, contractDetails));
 	LOG_TRACE_TAG(WRAPPER, "Received contract details for reqId: {}, symbol: {}, secType: {}, exchange: {}",
 	              reqId, contractDetails.contract.symbol, contractDetails.contract.secType,
 	              contractDetails.contract.exchange);
 }
 
 void ResponseWrapper::contractDetailsEnd(int reqId) {
-	auto *contracts =
-			obj_.try_get<ContractDetailsTag, std::vector<ContractDetails> >();
-
-	if (!contracts) {
-		LOG_WARN_TAG(WRAPPER, "Contract details end received for reqId {}, but no contract details were stored.",
-		             reqId);
-
-		hub_.send<ContractDetailsTag>(std::vector<ContractDetails>{});
-		return;
+	if (obj_.contains<ContractDetailsTag>()) {
+		const auto contracts = obj_.try_get<ContractDetailsTag, std::vector<ContractDetails> >();
+		hub_.send<ContractDetailsTag>(contracts ? std::optional(*contracts) : std::nullopt);
 	}
 
-	hub_.send<ContractDetailsTag>(*contracts);
 	LOG_DEBUG_TAG(WRAPPER, "Finished receiving contract details for reqId: {}", reqId);
 }
--- test/contract_details.cpp
+++ test/contract_details.cpp
@@ -18,10 +18,11 @@ void printContractMsg(const Contract& contract) {
 	printf("\tTradingClass: %s\n", contract.tradingClass.c_str());
 }
 
-int main() {
-	// Do a subscription to account summary and account update, then cancel the subscription
+void sync_method() {
 	const auto ib = std::make_unique<IB>();
 	auto ok = ib->connect();
+
+	if (!ok) throw "Connection failed!";
 
 	// We typically want to request a contract to get all details of it.
 	// We find the complete contract starting of a sub-set of the infos.
@@ -29,12 +30,12 @@ int main() {
 	contract.symbol = "AAPL";
 	contract.secType = "STK";
 	contract.exchange = "SMART";
-	// contract.currency = "USD";
+	// contract.currency = "USD";	// -> We expect to find more than one contract!
 
 	// We can request contract details that have contract information AND other infos,
 	// Otherwise, it's possible to directly fetch contracts
 	// auto result = ib->getContractDetails(1, contract);
-	auto contracts = ib->getContracts(1, contract);
+	auto contracts = ib->getContractsSync(1, contract);
 
 	if (contracts.has_value()) {
 		std::cout << contracts.value().size() << " Contracts have been found!" << std::endl;
@@ -46,4 +47,32 @@ int main() {
 	}
 
 	std::this_thread::sleep_for(std::chrono::seconds(10));
+}
+
+void async_method() {
+	const auto ib = std::make_unique<IB>();
+	auto ok = ib->connect();
+
+	if (!ok) throw "Connection failed!";
+
+	Contract contract;
+	contract.symbol = "AAPL";
+	contract.secType = "STK";
+	contract.exchange = "SMART";
+	contract.currency = "USD";	// -> We expect to find one contract!
+
+	ib->registerContract(contract);
+
+	std::cin.get();
+
+	const auto contract_details = ib->getContractDetails(contract);
+	const auto contract_result = ib->getContract(contract);
+
+	std::cout << contract_details.has_value() << std::endl;
+	std::cout << contract_result.has_value() << std::endl;
+}
+
+int main() {
+	async_method();
+	sync_method();
 }
